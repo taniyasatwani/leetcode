6b67590cba68748d3ed7f89cd88b17f882dbd073
taniya <taniya.satwani1998@gmail.com>
2020-08-03 00:57:21 +0530

    aug3

---
+++ hashset.py
@@ -0,0 +1,28 @@
+class MyHashSet(object):
+
+    def __init__(self):
+        self.set=[]
+        
+
+    def add(self, key):
+        if not self.contains(key):
+            self.set.append(key)
+        
+
+    def remove(self, key):
+        try:
+            self.set.remove(key)
+        except:
+            pass
+        
+
+    def contains(self, key):
+        return key in self.set
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
